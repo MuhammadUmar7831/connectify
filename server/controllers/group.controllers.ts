019beb18b9cdf198b6b9f0e63be7b7a3bf3cabbd
Muhammad Rayyan Asghar <rayyanasghar9@gmail.com>
2024-07-08 16:46:00 +0500

    "slight change"

--- server/controllers/group.controllers.ts
+++ server/controllers/group.controllers.ts
@@ -1,21 +1,17 @@
 import { NextFunction, Request, Response } from "express";
-import { QueryError, QueryResult, RowDataPacket, ResultSetHeader} from "mysql2";
+import { QueryError, QueryResult, RowDataPacket} from "mysql2";
 import connection from "../config/db";
 import errorHandler from "../errors/error";
 import { authRequest } from "../middlewares/authenticate";
 
 export const deleteGroup = async (req: authRequest, res: Response, next: NextFunction) => {
-  const {GroupId } = req.body;
-  const query = `DELETE FROM _Groups  WHERE GroupId = ?;`;
-  const values: any = [GroupId];
+  const groupId = req.body;
+  const query = `DELETE FROM _Groups WHERE GroupId = ?;`;
+  const values: any = [groupId];
 
-  connection.query(query, values, (err: QueryError | null, result: ResultSetHeader) => {
+  connection.query(query, values, (err: QueryError | null, result: any) => {
     if (err) {
       return next(err);
-    }
-    if (result.affectedRows === 0) {
-        console.log("Error")
-      return next(errorHandler(404, "Group not found or you do not have permission to delete it"));
     }
     res
       .status(200)
