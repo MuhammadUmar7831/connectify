992af7b83acfdfb8c480438a8bd22c5b8060169f
Muhammad Rayyan Asghar <rayyanasghar9@gmail.com>
2024-07-09 11:59:01 +0500

    "updated routes"

--- server/routes/group.routes.ts
+++ server/routes/group.routes.ts
@@ -3,11 +3,12 @@ import authenticate from "../middlewares/authenticate";
 import isAdmin from "../middlewares/isAdmin";
 import tryCatch from "../middlewares/tryCatch";
 import { deleteGroup } from "../controllers/group.controllers";
+import { getCommonGroups } from "../controllers/group.controllers";
 
 
 const router = Router();
 
-// router.get('/get/common/:friendId', authenticate, tryCatch(getCommonGroups)); //route to get all groups both users have joined (friendId is userId)
+router.get('/get/common/:friendId', authenticate, tryCatch(getCommonGroups)); //route to get all groups both users have joined (friendId is userId)
 router.delete('/delete', authenticate, isAdmin, tryCatch(deleteGroup)); //route to delete group chats (only admins can do this)
 // router.delete('/kick', authenticate, isAdmin, tryCatch(kickUser)); //route to kick a user from group (only admins can do this)
 // router.post('/create', authenticate, tryCatch(createGroup)); //route to create a group (user who create is admin)
