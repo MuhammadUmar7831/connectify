cf0657deaa7f1df6e6f6e1b844cbbd1281104c91
Muhammad Rayyan Asghar <rayyanasghar9@gmail.com>
2024-07-09 11:59:20 +0500

    "Added Find common friends function"

--- server/controllers/group.controllers.ts
+++ server/controllers/group.controllers.ts
@@ -18,3 +18,22 @@ export const deleteGroup = async (req: authRequest, res: Response, next: NextFun
       .send({ success: true, message: 'Group Deleted' });
   });
 };
+
+export const getCommonGroups = async (req: authRequest, res: Response, next: NextFunction)=>{
+  const friendId = req.params.friendId
+  const userId = req.userId
+
+  const query = `SELECT * FROM _Groups g JOIN GroupChats gc on g.GroupId = gc.GroupId JOIN Members m1 on gc.chatId = m1.chatId JOIN Members m2 on gc.chatId = m2.chatId WHERE m1.userId = ? AND m2.userId = ?;`
+  const values = [friendId,userId]
+  connection.query(query,values,(err: QueryError|null, result: RowDataPacket[])=>{
+      if(err){
+        return next(err)
+      }
+
+      if(result.length === 0){
+        res.status(403).send({success: false, message: "No Common Groups", data: null})
+      }
+
+      res.status(200).send({success: true, message:"Common Groups Found" , data: result})
+  })
+}
