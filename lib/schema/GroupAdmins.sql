6012367de19a56c63a49e177fe306008bcafa9ca
Muhammad Umar <mu8494759@gmail.com>
2024-07-13 21:37:22 +0500

    Group Admins Table Constaint

--- lib/schema/GroupAdmins.sql
+++ lib/schema/GroupAdmins.sql
@@ -1,6 +1,6 @@
 CREATE TABLE GroupAdmins (
     UserId INT,
-    GroupId INT,
+    GroupId INT UNIQUE,
     PRIMARY KEY (UserId, GroupId),
     FOREIGN KEY (UserId) REFERENCES Users(UserId) ON DELETE CASCADE,
     FOREIGN KEY (GroupId) REFERENCES _Groups(GroupId) ON DELETE CASCADE
