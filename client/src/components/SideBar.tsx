f5ffc63462e7455449fb998fe96837df0803b90f
Rabi Uddin <rabiuddin1@gmail.com>
2024-07-13 16:00:01 +0500

    Changed Icons in the sidebar

--- client/src/components/SideBar.tsx
+++ client/src/components/SideBar.tsx
@@ -1,33 +1,25 @@
-import { SiGooglegemini } from "react-icons/si";
-import { HiOutlineMail } from "react-icons/hi";
-import { IoMdChatboxes } from "react-icons/io";
-import { MdNotificationsNone } from "react-icons/md";
-import { RiDeleteBinLine } from "react-icons/ri";
+import { GrConnect } from "react-icons/gr";
+import { HiMiniUserGroup } from "react-icons/hi2";
+import ArchiveIcon from "../interface/icons/ArchiveIcon";
+import ChatIcon from "../interface/icons/ChatIcon";
 // import { VscDebugDisconnect } from "react-icons/vsc";
 import UserIcon from "../interface/icons/UserIcon";
 
 export default function SideBar() {
   return (
     <div className="bg-black h-full w-20 rounded-2xl flex flex-col items-center justify-between py-6 min-w-20">
-      <SiGooglegemini color="#FF4D0C" className="w-7 h-7" />
+      <GrConnect color="#FF4D0C" className="w-7 h-7" />
       <div>
         {
-          <ul className="flex flex-col justify-between h-48 mb-12">
+          <ul className="flex flex-col justify-between h-36 mb-12">
             <li>
-              {" "}
-              <HiOutlineMail className="w-5 h-5 cursor-pointer text-white hover:text-orange focus:text-orange" />
+              <ChatIcon className="w-5 h-5 cursor-pointer text-white hover:text-orange focus:text-orange" />
             </li>
             <li>
-              {" "}
-              <IoMdChatboxes className="w-5 h-5 cursor-pointer text-white hover:text-orange focus:text-orange" />
+              <HiMiniUserGroup className="w-5 h-5 cursor-pointer text-white hover:text-orange focus:text-orange" />
             </li>
             <li>
-              {" "}
-              <MdNotificationsNone className="w-5 h-5 cursor-pointer text-white hover:text-orange focus:text-orange" />
-            </li>
-            <li>
-              {" "}
-              <RiDeleteBinLine className="w-5 h-5 cursor-pointer text-white hover:text-orange focus:text-orange" />
+              <ArchiveIcon className="w-5 h-5 cursor-pointer text-white hover:text-orange focus:text-orange" />
             </li>
           </ul>
         }
