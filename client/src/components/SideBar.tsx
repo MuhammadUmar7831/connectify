e7aa27a784d724a7bf1100b9b0282a9cb39f2fed
Muhammad Umar <mu8494759@gmail.com>
2024-07-13 19:43:27 +0500

    Logo Color Hard Code undo

--- client/src/components/SideBar.tsx
+++ client/src/components/SideBar.tsx
@@ -8,7 +8,7 @@ import UserIcon from "../interface/icons/UserIcon";
 export default function SideBar() {
   return (
     <div className="bg-black h-full w-20 rounded-2xl flex flex-col items-center justify-between py-6 min-w-20">
-      <GrConnect color="#FF4D0C" className="w-7 h-7" />
+      <GrConnect className="w-7 h-7 text-orange hover:text-white cursor-pointer" />
       <div>
         {
           <ul className="flex flex-col justify-between h-36 mb-12">
