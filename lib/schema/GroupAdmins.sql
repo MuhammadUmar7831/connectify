694565a4cf389fd2a7f945c5405fd45b7c026734
Muhammad Umar <mu8494759@gmail.com>
2024-07-15 22:22:07 +0500

    Unique Constraint Drop From Group Admins (Because A Group van have more than 1 Admin)

--- lib/schema/GroupAdmins.sql
+++ lib/schema/GroupAdmins.sql
@@ -1,6 +1,6 @@
 CREATE TABLE GroupAdmins (
     UserId INT,
-    GroupId INT UNIQUE,
+    GroupId INT,
     PRIMARY KEY (UserId, GroupId),
     FOREIGN KEY (UserId) REFERENCES Users(UserId) ON DELETE CASCADE,
     FOREIGN KEY (GroupId) REFERENCES _Groups(GroupId) ON DELETE CASCADE
