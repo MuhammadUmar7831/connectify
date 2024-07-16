3e58f310c50b44e4e7da2098764cb1322ed90bd5
Muhammad Umar <mu8494759@gmail.com>
2024-07-16 17:26:47 +0500

    Get Archive Chats

--- server/controllers/chat.controllers.ts
+++ server/controllers/chat.controllers.ts
@@ -3,6 +3,7 @@ import { authRequest } from "../middlewares/authenticate";
 import connection from "../config/db";
 import { QueryError, QueryResult, RowDataPacket } from "mysql2";
 import errorHandler from "../errors/error";
+import { getGroupChatsQuery, getPersonalChatQuery } from "../utils/getChatQuries";
 
 export const createPersonalChat = async (req: authRequest, res: Response, next: NextFunction) => {
     const { content, receiverId } = req.body;
@@ -70,47 +71,14 @@ export const archiveChat = async (req: authRequest, res: Response, next: NextFun
 }
 
 export const getPersonalChats = async (req: authRequest, res: Response, next: NextFunction) => {
-    const query = `
-    SELECT DISTINCT 
-    c.ChatId,
-    u.Name, 
-    u.Avatar, 
-    CASE 
-        WHEN mg_all.SenderId = ? THEN 'You' 
-        ELSE u.Name 
-    END AS SenderName,
-    mg_all.MessageId, 
-    mg_all.SenderId, 
-    mg_all.Content, 
-    mg_all.TimeStamp, 
-    IFNULL(received_counts.received_count, 0) AS unSeenMessages
-    FROM Chats c
-    LEFT JOIN Members m ON c.ChatId = m.ChatId
-    LEFT JOIN Users u ON m.UserId = u.UserId
-    LEFT JOIN (
-        SELECT mg1.ChatId, max(mg1.MessageId) AS MessageId
-        FROM Messages mg1
-        GROUP BY ChatId
-    ) AS mg ON c.ChatId = mg.ChatId
-    LEFT JOIN Messages mg_all ON mg.MessageId = mg_all.MessageId
-    LEFT JOIN (
-        SELECT mg2.ChatId, COUNT(*) AS received_count
-        FROM Messages mg2
-        JOIN MessagesStatus mgs ON mg2.MessageId = mgs.MessageId
-        WHERE (mgs.Status = 'received' OR mgs.Status = 'sent') AND mgs.UserId = ?
-        GROUP BY mg2.ChatId
-    ) AS received_counts ON c.ChatId = received_counts.ChatId
-    WHERE 
-    c.Type = 'Group' AND
-    u.UserId != 1 AND
+    const sql = `${getPersonalChatQuery}  AND
     c.ChatId NOT IN (
         select ChatId from PinnedChats Where UserId = ?
     ) AND 
     c.ChatId NOT IN (
         SELECT ChatId FROM ArchivedChats WHERE UserId = ?
-    );`;
-
-    connection.query(query, [req.userId, req.userId, req.userId, req.userId], (err: QueryError | null, result: RowDataPacket[]) => {
+    );`
+    connection.query(sql, [req.userId, req.userId, req.userId, req.userId, req.userId], (err: QueryError | null, result: RowDataPacket[]) => {
         if (err) { return next(err); }
 
         res.status(200).send({ success: true, message: 'Personal Chats retrieved', data: result })
@@ -120,44 +88,15 @@ export const getPersonalChats = async (req: authRequest, res: Response, next: Ne
 
 
 export const getGroupChats = async (req: authRequest, res: Response, next: NextFunction) => {
-    const query = `
-    SELECT DISTINCT 
-    g.ChatId,
-    g.Name, 
-    g.Avatar, 
-    CASE 
-        WHEN mg_all.SenderId = ? THEN 'You' 
-        ELSE u.Name 
-    END AS SenderName,
-    mg_all.MessageId, 
-    mg_all.SenderId, 
-    mg_all.Content, 
-    mg_all.TimeStamp, 
-    IFNULL(received_counts.received_count, 0) AS unSeenMessages
-    FROM _Groups g
-    LEFT JOIN (
-        SELECT mg1.ChatId, max(mg1.MessageId) AS MessageId
-        FROM Messages mg1
-        GROUP BY ChatId
-    ) AS mg ON g.ChatId = mg.ChatId
-    LEFT JOIN Messages mg_all ON mg.MessageId = mg_all.MessageId
-    LEFT JOIN Users u ON mg_all.SenderId = u.UserId
-    LEFT JOIN (
-        SELECT mg2.ChatId, COUNT(*) AS received_count
-        FROM Messages mg2
-        JOIN MessagesStatus mgs ON mg2.MessageId = mgs.MessageId
-        WHERE (mgs.Status = 'received' OR mgs.Status = 'sent') AND mgs.UserId = ?
-        GROUP BY mg2.ChatId
-    ) AS received_counts ON g.ChatId = received_counts.ChatId
+    const sql = `${getGroupChatsQuery}
     WHERE 
     g.ChatId NOT IN (
         select ChatId from PinnedChats Where UserId = ?
     ) AND 
     g.ChatId NOT IN (
         SELECT ChatId FROM ArchivedChats WHERE UserId = ?
-    );`;
-
-    connection.query(query, [req.userId, req.userId, req.userId, req.userId], (err: QueryError | null, result: RowDataPacket[]) => {
+    )`
+    connection.query(sql, [req.userId, req.userId, req.userId, req.userId], (err: QueryError | null, result: RowDataPacket[]) => {
         if (err) {
             return next(err);
             // console.error('Error executing query:', err);
@@ -169,77 +108,26 @@ export const getGroupChats = async (req: authRequest, res: Response, next: NextF
 
 }
 export const getArchivedChats = (req: authRequest, res: Response, next: NextFunction) => {
+    
     const sqlQuery = `
-    select distinct 
-    NULL as MessageId,
-        pc.ChatId as chatId, 
-        (select count(mgs.MessageId)
-         from MessagesStatus mgs 
-         join Messages mg2 on mgs.MessageId = mg2.MessageId 
-         where mgs.Status = 'received' 
-         and mg2.ChatId = pc.ChatId 
-         and mgs.UserId = ?) as unSeenMessages,
-        ch.Type as chatType,
-        u.UserId as id,
-        u.Bio as description,
-        NULL as groupDateCreated,
-        NULL as groupdAdmin,
-        mg.Content as lastMessage,
-        u.Name as name, 
-        u.Avatar as avatar,
-        u.LastSeen as userLastSeen,
-        u.IsActivePrivacy as IsActivePrivacy,
-        u.IsLastSeenPrivacy as IsLastSeenPrivacy
-    from ArchivedChats ac 
-    join PersonalChats pc on pc.ChatId = ac.ChatId 
-    join Members m on m.ChatId = pc.ChatId
-    join Users u on u.UserId = m.UserId 
-    join Messages mg on mg.Messageid = pc.Messageid 
-    join Chats ch on ch.ChatId = pc.ChatId
-    where ac.UserId = ? 
-      and mg.Messageid = (select max(messageId) 
-                          from Messages mg2 
-                          where mg2.ChatId = pc.ChatId) 
-      and u.UserId != ?
+        ${getPersonalChatQuery} AND
+        c.ChatId NOT IN (
+        SELECT ChatId FROM PinnedChats Where UserId = ?
+        ) AND 
+        c.ChatId IN (
+            SELECT ChatId FROM ArchivedChats WHERE UserId = ?
+        )
+        UNION
+        ${getGroupChatsQuery}
+        WHERE 
+        g.ChatId NOT IN (
+            select ChatId from PinnedChats Where UserId = ?
+        ) AND 
+        g.ChatId IN (
+            SELECT ChatId FROM ArchivedChats WHERE UserId = ?
+        );`;
 
-    UNION
-
-    select distinct 
-        mg.MessageId as MessageId,
-        gc.ChatId as chatId,
-        (select count(mgs.MessageId)
-         from MessagesStatus mgs 
-         join Messages mg2 on mgs.MessageId = mg2.MessageId 
-         where mgs.Status = 'received' 
-         and mg2.ChatId = gc.ChatId 
-         and mgs.UserId = ?) as unSeenMessages,
-        ch.Type as chatType,
-        gr.GroupId as id,
-        gr.Description as description,
-        gr.DateCreated as groupDateCreated,
-        u.Name as groupdAdmin,
-        mg.Content as lastMessage, 
-        gr.Name as name,
-        gr.Avatar as avatar,
-        NULL as userLastSeen,
-        NULL as IsActivePrivacy,
-        NULL as IsLastSeenPrivacy
-    from ArchivedChats ac 
-    join GroupChats gc on gc.ChatId = ac.ChatId 
-    join Chats ch on ch.ChatId = ac.ChatId
-    join Members m on m.ChatId = gc.ChatId 
-    join _Groups gr on gr.GroupId = gc.GroupId 
-    join Messages mg on mg.Messageid = gc.Messageid 
-    join MessagesStatus mgs on mgs.MessageId = mg.MessageId 
-    join Users u on u.UserId = gr.CreatedBy
-    where ac.UserId = ? 
-      and mg.Messageid = (select max(messageId) 
-                          from Messages mg2 
-                          where mg2.ChatId = gc.ChatId)
-    group by mg.MessageId,gr.GroupId, gr.Description, gr.DateCreated, u.Name, gc.ChatId, ch.Type, mg.Content, gr.Name, gr.Avatar, gr.CreatedBy;
-    `;
-
-    connection.query(sqlQuery, [req.userId, req.userId, req.userId, req.userId, req.userId, req.userId, req.userId,], (err: QueryError | null, result: RowDataPacket[]) => {
+    connection.query(sqlQuery, [req.userId, req.userId, req.userId, req.userId, req.userId, req.userId, req.userId, req.userId, req.userId], (err: QueryError | null, result: RowDataPacket[]) => {
         if (err) {
             return next(err);
         }
