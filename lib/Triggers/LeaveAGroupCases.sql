faf02df0168c01b67367fd22a74819ced437c6c5
Muhammad Umar <mu8494759@gmail.com>
2024-07-18 01:44:22 +0500

    Bug Fix | Leave a Group Cases Now Oly Run for Group Chats

--- lib/Triggers/LeaveAGroupCases.sql
+++ lib/Triggers/LeaveAGroupCases.sql
@@ -5,37 +5,46 @@
 
 DELIMITER //
 
-CREATE TRIGGER before_member_delete
+CREATE TRIGGER leave_group
 BEFORE DELETE ON Members
 FOR EACH ROW
 BEGIN
+    DECLARE chat_type ENUM('Personal', 'Group');
     DECLARE member_count INT;
     DECLARE admin_count INT;
     DECLARE only_admin INT;
 
-    -- Check if the user is the only member of the group
-    SELECT COUNT(*) INTO member_count 
-    FROM Members 
+    -- Get the type of the chat
+    SELECT Type INTO chat_type
+    FROM Chats
     WHERE ChatId = OLD.ChatId;
 
-    IF member_count <= 1 THEN
-        SIGNAL SQLSTATE '45000' 
-        SET MESSAGE_TEXT = 'Cannot leave the group. Please delete the group instead.';
-    END IF;
+    -- Only execute the rest of the logic if the chat type is 'Group'
+    IF chat_type = 'Group' THEN
+        -- Check if the user is the only member of the group
+        SELECT COUNT(*) INTO member_count 
+        FROM Members 
+        WHERE ChatId = OLD.ChatId;
 
-    -- Check if number of admin of the group
-    SELECT COUNT(*) INTO admin_count 
-    FROM GroupAdmins 
-    WHERE GroupId = ( SELECT GroupId FROM _Groups WHERE ChatId = OLD.ChatId );
-    
-    IF admin_count = 1 THEN
-		-- check if that only admin is the user
-		SELECT UserId INTO only_admin
-        FROM GroupAdmins
+        IF member_count = 1 THEN
+            SIGNAL SQLSTATE '45000' 
+            SET MESSAGE_TEXT = 'Cannot leave the group. Please delete the group instead.';
+        END IF;
+
+        -- Check the number of admins of the group
+        SELECT COUNT(*) INTO admin_count 
+        FROM GroupAdmins 
         WHERE GroupId = (SELECT GroupId FROM _Groups WHERE ChatId = OLD.ChatId);
-        IF only_admin = OLD.UserId THEN
-			SIGNAL SQLSTATE '45000' 
-			SET MESSAGE_TEXT = 'Cannot leave the group as the only admin. Please assign another admin before leaving.';
+        
+        IF admin_count = 1 THEN
+            -- Check if that only admin is the user
+            SELECT UserId INTO only_admin
+            FROM GroupAdmins
+            WHERE GroupId = (SELECT GroupId FROM _Groups WHERE ChatId = OLD.ChatId);
+            IF only_admin = OLD.UserId THEN
+                SIGNAL SQLSTATE '45000' 
+                SET MESSAGE_TEXT = 'Cannot leave the group as the only admin. Please assign another admin before leaving.';
+            END IF;
         END IF;
     END IF;
 
