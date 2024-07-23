7b10fac15c62e992a7e08ec190977a535f999db3
Muhammad-Daniyal-Waseem <daniyalwaseem0808@gmail.com>
2024-07-23 21:11:49 +0500

    ChatArea | ChatSection | ChatHeader for PersonalChats completed

--- client/src/components/ChatArea/ChatSection.tsx
+++ client/src/components/ChatArea/ChatSection.tsx
@@ -1,13 +1,59 @@
 import Message from "./Message";
+import MessageReply from "./MessageReply";
+interface Message {
+  MessageId: number;
+  ChatId: number;
+  SenderId: number;
+  Content: string;
+  Timestamp: string;
+  Sender: string;
+  UserStatus: {
+    Status: string;
+    UserId: number;
+    UserName: string;
+  }[];
+  ReplyId: number | null;
+  ReplyContent: string | null;
+  ReplySenderId: number | null;
+  ReplySender: string | null;
+}
 
-export default function ChatSection() {
+interface ChatSectionProps {
+  message: Message[];
+  userId:string;
+}
+
+
+export default function ChatSection({message,userId}:ChatSectionProps) {
+
+   function getMessage(messageId:number):Message|undefined{
+  return  message.find(mg => mg.MessageId === messageId);
+  }
+
+  function formatTime(timestamp: string): string {
+    const date = new Date(timestamp);
+    let hours = date.getHours();
+    const minutes = date.getMinutes();
+    const ampm = hours >= 12 ? 'PM' : 'AM';
+    hours = hours % 12;
+    hours = hours ? hours : 12; // the hour '0' should be '12'
+    const minutesStr = minutes < 10 ? '0' + minutes : minutes;
+    const strTime = hours + ':' + minutesStr + ' ' + ampm;
+    return strTime;
+  }
+  const sortedMessages = [...message].sort((a, b) => a.MessageId - b.MessageId);
     return (
         <div className="bg-white rounded-2xl w-full h-full flex flex-col gap-10 p-4 overflow-y-scroll no-scrollbar">
-            <Message me={true} content="Hello! OK Yarism" time="5:19 PM" status="seen" senderName="Muhammad Umar" />
-            <Message me={false} content="Kam az Kam 30 saal ka time de" time="7:28 PM" status="seen" senderName="Rabi ud Din" />
-            <Message me={true} content="Ye Babu Rao ka style he" time="9:45 PM" status="received" senderName="Muhammad Umar" />
-            <Message me={false} content="Mast Plan He" time="10:50 PM" status="received" senderName="Muhammad Khizar" />
-            <Message me={false} content="Mu Se Supari Nikal ke Baat Kar" time="10:55 PM" status="sent" senderName="Huzaifa Rizwan" />
+          {
+            sortedMessages.map((m)=>(
+              m.ReplyId === null ? 
+             <Message me={m.SenderId===userId? true:false} content={m.Content} time={`${formatTime(m.Timestamp)}`} status={m.UserStatus[0].Status} senderName={m.Sender} />
+             :
+             <MessageReply me={m.SenderId===userId? true:false} message={getMessage(m.ReplyId)?.Content || ''}  content={m.Content} time={`${formatTime(m.Timestamp)}`} status={m.UserStatus[0].Status} senderName={m.Sender} />
+            ))
+          }  
+          
+           
         </div>
     )
 }
