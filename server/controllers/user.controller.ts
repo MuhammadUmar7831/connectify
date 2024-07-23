795baf54b076f05d1d8e992cab3b8a81684574b4
MuhammadUmar7831 <mu8494759@gmail.com>
2024-07-23 14:03:54 +0500

    get personal info of user (friend) route

--- server/controllers/user.controller.ts
+++ server/controllers/user.controller.ts
@@ -1,7 +1,8 @@
-import { NextFunction, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { authRequest } from "../middlewares/authenticate";
 import connection from "../config/db";
 import { QueryError, RowDataPacket } from "mysql2";
+import errorHandler from "../errors/error";
 
 export const getUser = async (req: authRequest, res: Response, next: NextFunction) => {
     const columns = 'UserId, Name, Email, Avatar, Bio, LastSeen, IsActivePrivacy, IsLastSeenPrivacy'
@@ -11,4 +12,19 @@ export const getUser = async (req: authRequest, res: Response, next: NextFunctio
 
         res.status(200).send({ success: true, message: 'User Retrieved', user: result[0] })
     })
+}
+
+export const getFriendInfo = async (req: Request, res: Response, next: NextFunction) => {
+    if (isNaN(parseInt(req.params.friendId))) {
+        return next(errorHandler(400, 'Invalid Request Params Expected friendId: type(number)'))
+    }
+    const query = `SELECT UserId, Name, Email, Avatar, Bio FROM Users WHERE UserId = ?`;
+    connection.query(query, [req.params.friendId], (err: QueryError | null, result: RowDataPacket[]) => {
+        if (err) { return next(err) }
+        if (result.length === 0) {
+            return next(errorHandler(404, 'No User Found'));
+        }
+        return res.status(200).send({ success: true, data: result[0] })
+    })
+
 }
--- server/routes/user.routes.ts
+++ server/routes/user.routes.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
-import { getUser } from "../controllers/user.controller";
+import { getFriendInfo, getUser } from "../controllers/user.controller";
 import authenticate from "../middlewares/authenticate";
 import tryCatch from "../middlewares/tryCatch";
 
 const router = Router();
 
-router.get('/get', authenticate, tryCatch(getUser)); //route to update user profile
+router.get('/get', authenticate, tryCatch(getUser)); //route to info user profile (logged in user)
+router.get('/get/friend/info/:friendId', tryCatch(getFriendInfo)); //route to get info of friend
 // router.put('/update', authenticate, tryCatch(updateProfile)); //route to update user profile
 // router.delete('/delete', authenticate, tryCatch(deleteProfile)); //route to delete user profile
 
