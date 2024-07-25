c2057ae6c7e17b10b7881dda6a3b098193db2d29
Rabi Uddin <rabiuddin1@gmail.com>
2024-07-26 00:31:25 +0500

    Changes made to the Info Skeleton

--- client/src/interface/skeletons/InfoListSkeleton.tsx
+++ client/src/interface/skeletons/InfoListSkeleton.tsx
@@ -2,16 +2,19 @@ import Skeleton from "./Skeleton";
 
 export const InfoListSkeleton = () => {
   return (
-    <div className="flex justify-start mt-8">
-      <div className="rounded-full w-11 h-11 overflow-hidden">
-        <Skeleton className="rounded-full w-14 h-12 bg-gray-200 animate-pulse" />
+    <div className="flex justify-between mt-7">
+      <div className="flex gap-2 w-full">
+        <Skeleton className="rounded-full w-12 h-11 bg-gray-200 animate-pulse object-contain" />
+        <div className="flex flex-col w-full">
+          <Skeleton className="rounded-md w-16 h-5 animate-pulse mt-1" />
+          <Skeleton className="rounded-md w-2/3 h-2 animate-pulse mt-2" />
+        </div>
       </div>
-      <div className="flex flex-col items-center ml-2 mr-96">
-        <Skeleton className="rounded-md w-16 h-5 animate-pulse mt-1"/>
-        <Skeleton className="rounded-md w-16 h-3 animate-pulse mt-2"/>
-      </div>
-      <div className="ml-96">
-        <Skeleton className="w-3 h-7 rounded-xl animate-pulse"/>
+
+      <div className="flex flex-col mr-2">
+        <Skeleton className="w-1 h-1 rounded-full animate-pulse mt-4" />
+        <Skeleton className="w-1 h-1 rounded-full animate-pulse mt-0.5" />
+        <Skeleton className="w-1 h-1 rounded-full animate-pulse mt-0.5" />
       </div>
     </div>
   );
--- client/src/interface/skeletons/InfoSkeleton.tsx
+++ client/src/interface/skeletons/InfoSkeleton.tsx
@@ -4,7 +4,7 @@ import { InfoListSkeleton } from "./InfoListSkeleton";
 export const InfoSkeleton = () => {
   return (
     <div className="w-2/3 min-w-[820px] h-full flex flex-col gap-2 overflow-y-scroll no-scrollbar">
-            <div className="rounded-2xl p-4">
+            <div className="bg-white rounded-2xl p-4">
                 <div className="rounded-full overflow-hidden mx-auto w-44 h-44">
                     <Skeleton className="size-44 rounded-full animate-pulse"/>
                 </div>
@@ -13,13 +13,13 @@ export const InfoSkeleton = () => {
                     <Skeleton className="w-32 h-4 rounded-md animate-pulse mt-2"/>
                 </div>
             </div>
-            <div className="rounded-2xl p-4">
+            <div className="bg-white rounded-2xl p-4">
                 <Skeleton className="w-20 h-5 mt-3 rounded-md animate-pulse"/>
-                <Skeleton className="w-56 h-2 mt-4 rounded-md animate-pulse"/>
+                <Skeleton className="w-1/3 h-2 mt-4 rounded-md animate-pulse"/>
                 <Skeleton className="w-20 h-5 mt-5 rounded-md animate-pulse"/>
-                <Skeleton className="w-56 h-2 mt-4 rounded-md animate-pulse"/>
+                <Skeleton className="w-2/3 h-2 mt-4 rounded-md animate-pulse"/>
             </div>
-            <div className="rounded-2xl p-4">
+            <div className="bg-white rounded-2xl p-4">
                 <Skeleton className="w-20 h-5 rounded-md animate-pulse mt-2"/>
                 <InfoListSkeleton/>
                 <InfoListSkeleton/>
