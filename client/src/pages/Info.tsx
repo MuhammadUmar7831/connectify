69316739b4e5f65903f04e8f841e9cdb2de5cbcd
MuhammadUmar7831 <mu8494759@gmail.com>
2024-08-06 17:26:33 +0500

    MyInfo structure initialized

--- client/src/pages/Info.tsx
+++ client/src/pages/Info.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import PersonalInfo from "../components/Info/Personal/PersonalInfo";
 import GroupInfo from "../components/Info/Group/GroupInfo";
+import MyInfo from "../components/Info/MyInfo/MyInfo";
 
 export default function Info() {
   return (
     <Routes>
       <Route path="/personal/:id" element={<PersonalInfo />} />
       <Route path="/group/:id" element={<GroupInfo />} />
+      <Route path="/my" element={<MyInfo />} />
     </Routes>
   )
 }
