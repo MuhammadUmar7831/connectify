016ac3d58fe719d112055dcee34026120275c103
MuhammadUmar7831 <mu8494759@gmail.com>
2024-08-26 16:02:50 +0500

    VITE_API_BASE_URL for socket

--- client/src/config/scoket.config.ts
+++ client/src/config/scoket.config.ts
@@ -3,7 +3,7 @@ import { io, Socket } from "socket.io-client";
 let socket: Socket | null = null;
 export const createSocket = (userId: number, chatIds: number[]) => {
     if (!socket || !socket.connected) {
-        socket = io(import.meta.env.API_BASE_URL || 'http://localhost:3000', {
+        socket = io(import.meta.env.VITE_API_BASE_URL || 'http://localhost:3000', {
             query: { userId, chatIds }
         })
     }
