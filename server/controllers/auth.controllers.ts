791431fee3659ef6658a7117c7134050b46acec7
MuhammadUmar7831 <mu8494759@gmail.com>
2024-08-26 15:10:02 +0500

    Cookies Https Issue

--- server/controllers/auth.controllers.ts
+++ server/controllers/auth.controllers.ts
@@ -1,9 +1,15 @@
-import { NextFunction, Request, Response } from "express";
-import { QueryError, QueryResult, RowDataPacket } from "mysql2";
+import { CookieOptions, NextFunction, Request, Response } from "express";
+import { QueryError, RowDataPacket } from "mysql2";
 import bcryptjs from "bcryptjs";
 import jwt from "jsonwebtoken";
 import connection from "../config/db";
 import errorHandler from "../errors/error";
+
+const COOKIE_OPTIONS: CookieOptions = {
+  httpOnly: true,
+  secure: process.env.NODE_ENV === 'production', // Ensure secure cookies in production
+  sameSite: 'lax' // Adjust based on your cross-site needs ('lax', 'strict', 'none')
+};
 
 export const signup = async (
   req: Request,
@@ -16,7 +22,7 @@ export const signup = async (
   const hashedPassword = bcryptjs.hashSync(password, salt);
 
   const query = `INSERT INTO Users (Name, Email, Password, Avatar, Bio, LastSeen) VALUES (?, ?, ?, ?, ?, ?)`;
-  const values: any = [name, email, hashedPassword, avatar, bio, lastSeen];
+  const values = [name, email, hashedPassword, avatar, bio, lastSeen];
 
   connection.query(query, values, (err: QueryError | null, result: any) => {
     if (err) {
@@ -35,52 +41,51 @@ export const signup = async (
       IsLastSeenPrivacy: 0,
     };
     res
-      .cookie("access_token", token, { httpOnly: true })
+      .cookie("access_token", token, COOKIE_OPTIONS)
       .status(201)
       .send({ success: true, message: "User Registered", user });
   });
 };
 
 export const signin = async (req: Request, res: Response, next: NextFunction) => {
-    const { email, password } = req.body;
-    const columns = 'u.UserId, Name, Email, Password, Avatar, Bio, LastSeen, IsActivePrivacy, IsLastSeenPrivacy, JSON_ARRAYAGG(m.ChatId) as ChatIds'
-    const sql = `SELECT ${columns} FROM Users u JOIN Members m ON u.UserId = m.UserId WHERE u.Email = ? GROUP BY u.UserId`;
-    connection.query(sql, [email], (err: QueryError | null, result: RowDataPacket[]) => {
-        if (err) { return next(err) }
+  const { email, password } = req.body;
+  const columns = 'u.UserId, Name, Email, Password, Avatar, Bio, LastSeen, IsActivePrivacy, IsLastSeenPrivacy, JSON_ARRAYAGG(m.ChatId) as ChatIds';
+  const sql = `SELECT ${columns} FROM Users u JOIN Members m ON u.UserId = m.UserId WHERE u.Email = ? GROUP BY u.UserId`;
 
-      if (result.length === 0) {
-        return next(errorHandler(404, "User not Found"));
-      }
+  connection.query(sql, [email], (err: QueryError | null, result: RowDataPacket[]) => {
+    if (err) {
+      return next(err);
+    }
 
-        const validPassword = bcryptjs.compareSync(password, result[0].Password);
-        if (validPassword) {
-            const token = jwt.sign({ email: email }, process.env.JWT_SECRET as string);
-            const user = {
-                UserId: result[0].UserId,
-                Name: result[0].Name,
-                Email: result[0].Email,
-                Avatar: result[0].Avatar,
-                Bio: result[0].Bio,
-                LastSeen: result[0].LastSeen,
-                IsActivePrivacy: result[0].IsActivePrivacy,
-                IsLastSeenPrivacy: result[0].IsLastSeenPrivacy,
-                ChatIds: result[0].ChatIds
-            }
-            res
-                .cookie('access_token', token, { httpOnly: true, secure: true })
-                .status(200)
-                .send({ success: true, message: `Wellcome ${result[0].Name}`, user });
-        } else {
-            return next(errorHandler(404, 'Incorrect Password'));
-        }
-    })
-}
+    if (result.length === 0) {
+      return next(errorHandler(404, "User not Found"));
+    }
 
-export const signout = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  res.clearCookie("access_token");
-  res.status(200).send({ success: true, message: "signout Successfully" });
+    const validPassword = bcryptjs.compareSync(password, result[0].Password);
+    if (validPassword) {
+      const token = jwt.sign({ email: email }, process.env.JWT_SECRET as string);
+      const user = {
+        UserId: result[0].UserId,
+        Name: result[0].Name,
+        Email: result[0].Email,
+        Avatar: result[0].Avatar,
+        Bio: result[0].Bio,
+        LastSeen: result[0].LastSeen,
+        IsActivePrivacy: result[0].IsActivePrivacy,
+        IsLastSeenPrivacy: result[0].IsLastSeenPrivacy,
+        ChatIds: result[0].ChatIds
+      };
+      res
+        .cookie('access_token', token, COOKIE_OPTIONS)
+        .status(200)
+        .send({ success: true, message: `Welcome ${result[0].Name}`, user });
+    } else {
+      return next(errorHandler(404, 'Incorrect Password'));
+    }
+  });
+};
+
+export const signout = async (req: Request, res: Response, next: NextFunction) => {
+  res.clearCookie("access_token", COOKIE_OPTIONS);
+  res.status(200).send({ success: true, message: "Signout Successfully" });
 };
