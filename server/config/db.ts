cb800d99dc840391f51d70c031fcf23ae9062873
MuhammadUmar7831 <mu8494759@gmail.com>
2024-08-26 14:59:07 +0500

    db connection using pool

--- server/config/db.ts
+++ server/config/db.ts
@@ -3,24 +3,29 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-function connectToDatabase() {
-    const connection = mysql.createConnection({
+function createDatabasePool() {
+    const pool = mysql.createPool({
         host: process.env.DB_HOST,
         port: 27674,
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME,
+        waitForConnections: true,
+        connectionLimit: 10,  // Adjust the connection limit based on your needs
+        queueLimit: 0
     });
 
-    connection.connect((error) => {
+    pool.getConnection((error, connection) => {
         if (error) {
             console.error(':( Error connecting to MySQL database:', error);
         } else {
             console.log(':) Connected to MySQL database!');
+            connection.release(); // Release the connection back to the pool
         }
     });
 
-    return connection;
+    return pool;
 }
-const connection = connectToDatabase();
-export default connection;
+
+const pool = createDatabasePool();
+export default pool;
