0f1d66c1f77db8f5a1a7a320a4241cf2b4572bc5
MuhammadUmar7831 <mu8494759@gmail.com>
2024-08-26 16:17:00 +0500

    with credientials key in socket

--- client/src/config/scoket.config.ts
+++ client/src/config/scoket.config.ts
@@ -4,7 +4,8 @@ let socket: Socket | null = null;
 export const createSocket = (userId: number, chatIds: number[]) => {
     if (!socket || !socket.connected) {
         socket = io(import.meta.env.VITE_API_BASE_URL || 'http://localhost:3000', {
-            query: { userId, chatIds }
+            query: { userId, chatIds },
+            withCredentials: true,
         })
     }
     return socket;
