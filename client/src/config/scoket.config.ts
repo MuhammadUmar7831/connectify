4aa2fe0a40298187ca7d6404f6b57aeb98386c52
MuhammadUmar7831 <mu8494759@gmail.com>
2024-08-26 16:30:03 +0500

    Socket Malfunction Issue

--- client/src/config/scoket.config.ts
+++ client/src/config/scoket.config.ts
@@ -4,12 +4,15 @@ let socket: Socket | null = null;
 export const createSocket = (userId: number, chatIds: number[]) => {
     if (!socket || !socket.connected) {
         socket = io(import.meta.env.VITE_API_BASE_URL || 'http://localhost:3000', {
-            query: { userId, chatIds },
-            withCredentials: true,
-        })
+            query: {
+                userId: userId.toString(),  // Ensure userId is a string
+                chatIds: chatIds.join(','), // Convert array to comma-separated string
+            },
+            withCredentials: true, // Ensure credentials are sent
+        });
     }
     return socket;
-}
+};
 
 export const getSocket = () => {
     return socket;
